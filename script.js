7eeec1cf975265765b7ba272b7f01b02cc695486
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-11-13 16:30:44 +0200

    add sound

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const elements = document.querySelectorAll('.element');
+  const sound = new Audio('sound.mp3');
 
   elements.forEach(element => {
     element.addEventListener('click', () => {
+       sound.play();
       element.style.opacity = '0';
       element.style.transform = 'scale(0.5)';
-      // setTimeout(() => {
-      //   element.style.display = 'none';
-      // }, 500);
+
     });
   });
 });
