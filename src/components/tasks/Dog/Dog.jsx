f745096be1f53d86958efb8b30fa45c0bd729a01
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-13 18:46:55 +0300

    add code for hver block dog

--- src/components/tasks/Dog/Dog.jsx
+++ src/components/tasks/Dog/Dog.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import clx from '../tasks.module.css';
 import taskVideo from '../../../video/tasks/dog.mp4';
-
-
+import dogHover from '../../../images/hover/dog.png';
+import questionSign from '../../../images/decor/question.gif';
 
 export const Dog = ({handleToAllTasksClick}) => {
 
     return (
-        <div className={clx.question}>
+        <div className={`${clx.question} ${clx.questionDog}`}>
             <video autoPlay id="dogVideo" controls className={clx.fullscreenVideo} loop>
                 <source src={taskVideo} type="video/mp4"/>
             </video>
@@ -19,6 +19,10 @@ export const Dog = ({handleToAllTasksClick}) => {
             >
                 Всі завдання
             </button>
+            <div className={clx.hoverBlock}>
+                <img src={dogHover} className={clx.hoverImg}/>
+                <img src={questionSign} className={clx.questionSign}/>
+            </div>
         </div>
     );
 };
