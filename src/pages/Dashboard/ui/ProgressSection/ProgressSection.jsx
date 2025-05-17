9ba216947fc6b76bd63d4994b74bbac995890116
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-17 21:14:35 +0300

    add code for 2 progress images

--- src/pages/Dashboard/ui/ProgressSection/ProgressSection.jsx
+++ src/pages/Dashboard/ui/ProgressSection/ProgressSection.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { ProgressBar } from "react-step-progress-bar";
 import clx from "./ProgressSection.module.css";
+import girl1 from  '../../../../shared/assets/images/girl/girl1.png'
+import girl18 from  '../../../../shared/assets/images/girl/girl18.png';
 
 export const ProgressSection = ({ progress }) => (
-    <div className={clx.progressbarWrapper}>
-        <ProgressBar
-            width="600px"
-            height="60px"
-            percent={progress}
-            filledBackground="#FF191B"
-        />
-    </div>
-);
+        <div className={clx.infoBlock}>
+            <div className={clx.progressbarWrapper}>
+                <ProgressBar
+                    width="600px"
+                    height="60px"
+                    percent={progress}
+                    filledBackground="#FF191B"
+                />
+
+            </div>
+            <div className={clx.imagesWrapper}>
+                <img src={girl1} alt="Girl 1 Photo" className={`${clx.progressImg} ${clx["progressImg__girl1"]}`}/>
+                <img src={girl18} alt="Girl 1 Photo" className={`${clx.progressImg} ${clx["progressImg__girl18"]}`}/>
+            </div>
+        </div>
+    )
+;
