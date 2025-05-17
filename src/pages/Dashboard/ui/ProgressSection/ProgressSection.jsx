f127c7d6c7774c992a04c835c54f128c9fd54f84
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-17 21:59:33 +0300

    add image appear

--- src/pages/Dashboard/ui/ProgressSection/ProgressSection.jsx
+++ src/pages/Dashboard/ui/ProgressSection/ProgressSection.jsx
@@ -8,6 +8,11 @@ export const ProgressSection = ({ progress }) =>
     const girlImages = Array.from({ length: 18 }, (_, i) =>
         require(`../../../../shared/assets/images/girl/girl${i + 1}.png`)
     );
+
+    const startProgress = 5;
+    const endProgress = 100;
+    const step = (endProgress - startProgress) / (girlImages.length - 1);
+
     return (
         <div className={clx.infoBlock}>
             <div className={clx.progressbarWrapper}>
@@ -20,14 +25,23 @@ export const ProgressSection = ({ progress }) =>
 
             </div>
             <div className={clx.imagesWrapper}>
-                {girlImages.map((imgSrc, index) => (
-                    <img
-                        key={index}
-                        src={imgSrc}
-                        alt={`Girl ${index + 1} Photo`}
-                        className={`${clx.progressImg} ${clx[`progressImg__girl${index + 1}`] || ''}`}
-                    />
-                ))}
+                {girlImages.map((imgSrc, index) => {
+                    const threshold = startProgress + index * step;
+                    const isVisible = progress >= threshold;
+
+                    return (
+                        <img
+                            key={index}
+                            src={imgSrc}
+                            alt={`Girl ${index + 1} Photo`}
+                            className={`
+                                ${clx.progressImg}
+                                ${clx[`progressImg__girl${index + 1}`] || ''}
+                                ${isVisible ? clx.fadeIn : clx.fadeOut}
+                            `}
+                        />
+                    );
+                })}
             </div>
         </div>
     )}
