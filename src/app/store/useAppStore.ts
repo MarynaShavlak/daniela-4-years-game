03696a2ccb95a16054edcddea89711b33c03acce
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-18 22:02:12 +0300

    ts for zustand store

--- src/app/store/useAppStore.ts
+++ src/app/store/useAppStore.ts
@@ -1,6 +1,23 @@
 import { create } from 'zustand';
+import { TaskSymbol } from '@/shared/utils/taskImages'; // assuming this is where TaskSymbol is defined
 
-export const useAppStore = create((set) => ({
+interface AppStore {
+    isRulesShown: boolean;
+    isDashboardShown: boolean;
+    isFullscreen: boolean;
+    hiddenSymbols: TaskSymbol[];
+
+    setShowRules: (val: boolean) => void;
+    setShowDashboard: (val: boolean) => void;
+    setIsFullscreen: (val: boolean) => void;
+    addHiddenSymbol: (symbol: TaskSymbol) => void;
+    resetHiddenSymbols: () => void;
+
+    showRules: () => void;
+    showDashboard: () => void;
+}
+
+export const useAppStore = create<AppStore>((set) => ({
     isRulesShown: false,
     isDashboardShown: false,
     isFullscreen: false,
@@ -11,7 +28,6 @@ export const useAppStore = create((set) => ({
     setIsFullscreen: (val) => set({ isFullscreen: val }),
     addHiddenSymbol: (symbol) => set((state) => ({ hiddenSymbols: [...state.hiddenSymbols, symbol] })),
     resetHiddenSymbols: () => set({ hiddenSymbols: [] }),
-
 
     showRules: () => set({ isRulesShown: true }),
     showDashboard: () => set({ isDashboardShown: true }),
