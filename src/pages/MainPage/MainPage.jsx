ce642ced05343fb6de3de3b1c688a98a9967be58
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-18 19:07:50 +0300

    add code for start play

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,32 +1,46 @@
 import clx from './MainPage.module.css';
-import React from 'react';
+import React, { useRef} from 'react';
 import { Button } from '../../shared/ui/Button/Button';
 import mainImg from '../../shared/assets/images/slides/main.jpg';
+import startAudio from '../../shared/assets/audio/start.mp3';
 
 export const MainPage = ({ onShowRules, isFullscreen, toggleFullscreen }) => {
-    return (
 
-            <div className={clx.initialView}>
-                <img src={mainImg} alt="Hero image" />
-                {!isFullscreen && (
-                    <Button
-                        size="l"
-                        onClick={toggleFullscreen}
-                        className={clx.initBtn}
-                    >
-                        Запуск гри
-                    </Button>
-                )}
-                {isFullscreen && (
-                    <Button
-                        size="l"
-                        onClick={onShowRules}
-                        className={clx.toRulesBtn}
-                    >
-                        Прослухати правила
-                    </Button>
-                )}
-            </div>
+    const audioRef = useRef(null);
+    const handleStartClick = () => {
+
+        if (audioRef.current) {
+            audioRef.current.play().catch((err) => {
+                console.error("Audio play failed:", err);
+            });
+        }
+        toggleFullscreen();
+    };
+    return (
+        <div className={clx.initialView}>
+            <img src={mainImg} alt="Hero image"/>
+            <audio ref={audioRef}>
+                <source src={startAudio} type="audio/mp3"/>
+            </audio>
+            {!isFullscreen && (
+                <Button
+                    size="l"
+                    onClick={handleStartClick}
+                    className={clx.initBtn}
+                >
+                    Запуск гри
+                </Button>
+            )}
+            {isFullscreen && (
+                <Button
+                    size="l"
+                    onClick={onShowRules}
+                    className={clx.toRulesBtn}
+                >
+                    Прослухати правила
+                </Button>
+            )}
+        </div>
 
     );
 };
