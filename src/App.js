dfe00f22733b07c5493a9ad9ac23f2e8d44a2a3f
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-18 18:17:40 +0300

    fix App structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
 
-
+import React, { useEffect, useRef, useState } from 'react';
+import screenfull from 'screenfull';
 import { MainPage } from './pages/MainPage/MainPage';
-
-
-
-
+import { Rules } from './pages/Rules/ui/Rules';
 
 function App() {
-    return <MainPage/>;
+    const appRef = useRef(null);
+    const [showRules, setShowRules] = useState(false);
+    const [isFullscreen, setIsFullscreen] = useState(false);
+
+    const toggleFullscreen = () => {
+        if (screenfull.isEnabled) {
+            screenfull.toggle(appRef.current);
+        }
+    };
+
+    useEffect(() => {
+        if (!screenfull.isEnabled) return;
+
+        const changeHandler = () => {
+            setIsFullscreen(screenfull.isFullscreen);
+        };
+
+        screenfull.on('change', changeHandler);
+
+        return () => {
+            screenfull.off('change', changeHandler);
+        };
+    }, []);
+
+    return (
+        <div ref={appRef}>
+            {showRules ? (
+                <Rules onBack={() => setShowRules(false)} />
+            ) : (
+                <MainPage
+                    onShowRules={() => setShowRules(true)}
+                    isFullscreen={isFullscreen}
+                    toggleFullscreen={toggleFullscreen}
+                />
+            )}
+        </div>
+    );
 }
 
 export default App;
